1ad8acd7effa14571832ad7d588ffafab1167698
tlaceby <tylerbadgertutorials@gmail.com>
2024-02-18 16:32:33 -0700

    Concurrent dev builds can now exit safely when app crashes or vite fails. Added some minor changes to scripts and preload setup. Added example to inside of App.svelte

--- renderer/src/App.svelte
+++ renderer/src/App.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
+  import ipc from "./ipc";
+
+  let electronVersion: string;
+  let nodeVersion: string;
+
+  $: nodeVersionTxt = nodeVersion ? `Node: ${nodeVersion}` : "Node Version";
+  $: electronVersionTxt = electronVersion ? `Electron: ${electronVersion}` : "Electron Version";
+
+  async function getVersion (opt: "electron" | "node") {
+    const version = await ipc.getVersion(opt);
+    
+    opt === "electron"? 
+      electronVersion = version :
+      nodeVersion = version;
+  }
+
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
+  <h1>Electron + Svelte</h1>
 
-  <div class="card">
-    <Counter />
+  <div class="buttons">
+    <button class="btn" on:click|once={() => getVersion("node")}>{nodeVersionTxt}</button>
+    <button class="btn" on:click|once={() => getVersion("electron")}>{electronVersionTxt}</button>
   </div>
+
 </main>
 
