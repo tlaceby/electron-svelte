57bb00c4a26b366115373670c4369bef63a6732b
tlaceby <tylerbadgertutorials@gmail.com>
2024-02-18 16:34:55 -0700

    Trailing files

--- electron/app.ts
+++ electron/app.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
 import { join } from "path";
 
 let mainWindow: BrowserWindow;
@@ -7,11 +7,11 @@ app.once("ready", main);
 
 async function main() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 650,
+    width: 700,
+    height: 600,
     show: false,
     webPreferences: {
-      devTools: !app.isPackaged,
+      devTools: true || !app.isPackaged,
       preload: join(__dirname, "preload.js"),
     },
   });
@@ -19,19 +19,13 @@ async function main() {
   if (app.isPackaged) {
     mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
   } else {
-    let conn = false;
-
-    while (!conn) {
-      try {
-        await mainWindow.loadURL(`http://localhost:5173/`);
-        conn = true;
-      } catch (_) {
-        await new Promise((res) => setTimeout(res, 100));
-      }
-    }
-
-    mainWindow.webContents.openDevTools({ mode: "detach" });
+    await mainWindow.loadURL(`http://localhost:5173/`);
+    // mainWindow.webContents.openDevTools({ mode: "detach" });
   }
 
   mainWindow.once("ready-to-show", mainWindow.show);
 }
+
+ipcMain.handle("get-version", (_, key: "electron" | "node") => {
+  return String(process.versions[key]);
+});
--- electron/preload.ts
+++ electron/preload.ts
@@ -1,7 +1,12 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
 
 export const CONTEXT_BRIDGE = {
-  foo: 45,
+  /**
+   * Returns the version from process.versions of the supplied target.
+   */
+  getVersion: async (opt: "electron" | "node"): Promise<string> => {
+    return await ipcRenderer.invoke(`get-version`, opt);
+  },
 };
 
 contextBridge.exposeInMainWorld("bridge", CONTEXT_BRIDGE);
